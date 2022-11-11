756a66e03b0edbcfca70d2ee08ec81483298d339
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2022-11-11 08:39:53 +0100

    Eval

---
+++ NET5NET6Kurs/ConsoleWithEFCoreSample/Data/SchoolContext.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleWithEFCoreSample.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWithEFCoreSample.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\\mssqllocaldb;Database=SchoolDB123;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
